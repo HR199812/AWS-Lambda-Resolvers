901be6cbe7096a64e4f2d376c12e74043103306c
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2022-06-03 19:14:55 +0530

    Hide Credentials

--- mongo_db_resolver/index.js
+++ mongo_db_resolver/index.js
@@ -3,7 +3,7 @@ const MongoClient = require("mongodb").MongoClient;
 
 // Define our connection string. Info on where to get this will be described below. In a real world application you'd want to get this string from a key vault like AWS Key Management, but for brevity, we'll hardcode it in our serverless function here.
 const MONGODB_URI =
-  "mongodb+srv://mongoresolver:mongoresolver@cluster0.ud4tf.mongodb.net/?retryWrites=true&w=majority";
+  "atlas_uri";
 
 // Once we connect to the database once, we'll store that connection and reuse it so that we don't have to connect to the database on every request.
 let cachedDb = null;
