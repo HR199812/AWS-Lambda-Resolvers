848356f02b9346dd347ca3fc9bef516b4502bcba
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2022-06-03 19:15:00 +0530

    Update handler.js

--- mongo_db_vpc_peering/handler.js
+++ mongo_db_vpc_peering/handler.js
@@ -1,26 +1,33 @@
-'use strict';
-const mongo = require('mongodb');
-const { MongoClient } = mongo;
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI =
+  "atlas_uri";
+
 let db = null;
 
-let connectToDatabase = (uri, dbName) => {
+let connectToDatabase = (dbName) => {
   if (db && db.serverConfig.isConnected()) {
     return Promise.resolve(db);
   }
-  return MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(client => {
+  return MongoClient.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then((client) => {
     db = client.db(dbName);
     return db;
   });
 };
 
-let getAllMovies = async (db, table) => {
-  return await db.collection(table).find({}).limit(20).toArray();
-}
+let getAllMovies = (db, table, query) => {
+  return db.collection(table).findOne(query);
+};
 
-exports.getData = async event => {
-  const dbConnection = await connectToDatabase("mongodb+srv://mongoresolver:mongoresolver@cluster0.ud4tf.mongodb.net/?retryWrites=true&w=majority", "sample_mflix");
-  const allMovies = await getAllMovies(dbConnection, "movies");
+exports.getData = async (event) => {
 
+  console.log(event);
+  console.log("Inside get data");
+  const dbConnection = await connectToDatabase("sample_mflix");
+  console.log(dbConnection);
+  const allMovies = await getAllMovies(dbConnection, "movies", {});
   console.log(allMovies);
 
   return {
