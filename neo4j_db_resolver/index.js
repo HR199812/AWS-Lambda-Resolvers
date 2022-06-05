1c84f7b07d0aced18fdf45f76f15799ea73e4fc3
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2022-06-05 15:16:28 +0530

    Create GraphQLResolver
    Created a graphql resolver for CRUD operations on Author and Book Database

--- neo4j_db_resolver/index.js
+++ neo4j_db_resolver/index.js
@@ -1,51 +1,35 @@
-(async () => {
-  const neo4j = require("neo4j-driver");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
-  const uri = "neo4j+s://a78a21f6.databases.neo4j.io";
-  const user = "reachout@capcons.com";
-  const password = "Capcons@2106";
+const app = express();
+const port = 3000;
+const MongoURI = `mongodb+srv://Capcons:b9v8FOHqUxjnnZiN@cluster0.ps2tt.mongodb.net/?retryWrites=true&w=majority`;
 
-  const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
-  const session = driver.session();
+// connect to mlab database
+// make sure to replace my db string & creds with your own
+mongoose
+  .connect(MongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {})
+  .catch((err) => console.log(err));
 
-  const person1Name = "Alice";
-  const person2Name = "David";
+mongoose.connection.once("open", () => {
+  console.log("conneted to database");
+});
 
-  try {
-    // To learn more about the Cypher syntax, see https://neo4j.com/docs/cypher-manual/current/
-    // The Reference Card is also a good resource for keywords https://neo4j.com/docs/cypher-refcard/current/
-    const writeQuery = `MERGE (p1:Person { name: $person1Name })
-                          MERGE (p2:Person { name: $person2Name })
-                          MERGE (p1)-[:KNOWS]->(p2)
-                          RETURN p1, p2`;
+// bind express with graphql
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
-    // Write transactions allow the driver to handle retries and transient errors
-    const writeResult = await session.writeTransaction((tx) =>
-      tx.run(writeQuery, { person1Name, person2Name })
-    );
-    writeResult.records.forEach((record) => {
-      const person1Node = record.get("p1");
-      const person2Node = record.get("p2");
-      console.log(
-        `Created friendship between: ${person1Node.properties.name}, ${person2Node.properties.name}`
-      );
-    });
-
-    const readQuery = `MATCH (p:Person)
-                         WHERE p.name = $personName
-                         RETURN p.name AS name`;
-    const readResult = await session.readTransaction((tx) =>
-      tx.run(readQuery, { personName: person1Name })
-    );
-    readResult.records.forEach((record) => {
-      console.log(`Found person: ${record.get("name")}`);
-    });
-  } catch (error) {
-    console.error("Something went wrong: ", error);
-  } finally {
-    await session.close();
-  }
-
-  // Don't forget to close the driver connection when you're finished with it
-  await driver.close();
-})();
+app.listen(port, () => {
+  console.log(`now listening for requests on port ${port}`);
+});
---
+++ neo4j_db_resolver/schema/schema.js
@@ -0,0 +1,141 @@
+const graphql = require("graphql");
+const Book = require("../models/book");
+const Author = require("../models/Author");
+const book = require("../models/book");
+// const _ = require('lodash');
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+} = graphql;
+
+// Book type object to be used in all CRUD operations related to Book collection
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return Author.findById(parent.authorId);
+      },
+    },
+  }),
+});
+
+// Author type object to be used in all CRUD operations related to Author collection
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return Book.find({ authorId: parent.id });
+      },
+    },
+  }),
+});
+
+// Query to be returned back to index/server file as schema is required to
+// perform operations on graph and therefore in the end save data to mongodb
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Book.findById(args.id);
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Author.findById(args.id);
+      },
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return Book.find({});
+      },
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return Author.find({});
+      },
+    },
+  },
+});
+
+// Mutation object is used to write queries for CUD on mongo database using GraphQL
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      resolve(parent, args) {
+        let author = new Author({
+          name: args.name,
+          age: args.age,
+        });
+        return author.save();
+      },
+    },
+    addBook: {
+      type: BookType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        let book = new Book({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId,
+        });
+        return book.save();
+      },
+    },
+    updateBook: {
+      type: BookType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        console.log(args.id);
+        book.findById(args.id).then((bookObj) => {
+          console.log(bookObj);
+          bookObj.name = args.name;
+          bookObj.genre = args.genre;
+
+          bookObj.save();
+        });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutation,
+});
